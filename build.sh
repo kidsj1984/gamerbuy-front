61e950d7fe867e20c238c3c56464293c9071e66a
单杰 <kidsj1984@github.com>
2025-10-26 11:04:27 +0800

    feat: 代码搬迁

--- build.sh
+++ build.sh
@@ -2,9 +2,12 @@
 
 git pull --force
 
+yarn
 yarn build:prod
 
-# rm -rf ../gamebug-fe-deloy/*
+rm -rf ../www.gamerbuy.cc/*
+cp -rf dist/* ../www.gamerbuy.cc/
+cp -rf src/static ../www.gamerbuy.cc/
 
 # cp -r dist/* ../gamebug-fe-deloy/
 # cp -r src/static ../gamebug-fe-deloy/
